a87deea0ee248fc104210f0d13e7df76cf4933e6
snaaz <snaaz021@gmail.com>
2017-05-10 18:25:30 +0530

    10th May

--- controller/update_profile.php
+++ controller/update_profile.php
@@ -7,25 +7,28 @@ $error3 = null;
 $error4 = null;
 $error5 = null;
 
+
 if (isset ( $_GET ['id'] )) {
 	$id = $_GET ['id'];
 	$users = mysqli_query ( $connection, "select * FROM `users` WHERE id='" . $id . "'" );
 	$row=mysqli_fetch_array($users);
 	$states = mysqli_query ( $connection, "select * from states order by sname" );
-	$state1=mysqli_query ( $connection, "select * from states where sname='".$row["state"]."'" );
-	$state2=mysqli_fetch_array($state1);
-	$districts = mysqli_query ( $connection, "select * from district where state_id = '" . $state2['id'] . "'" );
-	$user_subjects = mysqli_query ( $connection, "select * from user_subjects where user_id = '" . $id . "'" );
+	$user_state=mysqli_query ( $connection, "select * from states where sname='".$row["state"]."'" );
+	$state=mysqli_fetch_array($user_state);
+	$districts = mysqli_query ( $connection, "select * from district where state_id = '" . $state['id'] . "'" );
+	$usersubject_names = mysqli_query ( $connection, "select subject_name from user_subjects where user_id = '" . $id . "'" );
 	$subjects = mysqli_query ( $connection, "select * from subjects" );
-	
 	$class = mysqli_query ( $connection, "select * from class" );
 	$pics = mysqli_query ( $connection, "SELECT * FROM profile_pic where user_id='" . $id . "'" );
 	
-
+	
+	$res= array();
+	while($sub=mysqli_fetch_array($usersubject_names)){
+		$res[] =$sub['subject_name'];
+	
+	}
 	
 }
-
-
 
 
 
@@ -49,10 +52,11 @@ if (isset ( $_POST ['update'] )) {
 	$months= $_POST['months'];
 	$experience = $years.'-'.$months;
 	
-	echo $subject;
+	//echo $subject;
 	$string = explode(',',$subject);
-	 $count=count($string);
-	//echo $string[2];
+	$count=count($string);
+//	echo $string[2];
+	
 	
 	$users = mysqli_query ( $connection, "select * from users where id != '$id' and (email='$email' or mobile='$mobile')   " );
 	$rows=mysqli_num_rows($users);
@@ -93,49 +97,51 @@ if (isset ( $_POST ['update'] )) {
 		$error1 = explode ( ";", $error1, 3 );
 		$error1 = array_values ( $error1 ) [0];
 	}
-	
+	}
 	$user_subjects = mysqli_query ( $connection, "select * from user_subjects where user_id='" . $id . "'" );
 	$user_subject = mysqli_fetch_array ( $user_subjects );
 	
-	if ($user_subject ['id']) {
+	
+	if (count($user_subject) > 0) {
 		
-		$update_subject = mysqli_query ( $connection, "UPDATE user_subjects set subject_name='" . $subject . "'
-		 WHERE user_id='" . $id . "'" );
+		$delete_subject = mysqli_query ( $connection, "delete from user_subjects where user_id='" . $id . "'");
 		
-		if (! $update_subject) {
-			$error2 = mysqli_error ( $connection );
-			$error2 = explode ( ";", $error2, 3 );
-			$error2 = array_values ( $error2 ) [0];
-		}
-	} else {
+	}
+	
+	
+		for($i=0 ; $i<$count ; $i++)
+		{
 		$insert_subject = mysqli_query ( $connection, "INSERT INTO user_subjects(subject_name,user_id) 
-				VALUES('$subject','$id')" );
+				VALUES('$string[$i]','$id')" );
+		}
 		if (! $insert_subject) {
 			$error3 = mysqli_error ( $connection );
 			$error3 = explode ( ";", $error3, 3 );
 			$error3 = array_values ( $error3 ) [0];
 		}
-	}
+	
 	
 	
 	$pic_type= array("gif","GIF","JPEG" ,"jpeg","jpg" ,"JPG");
-	if (($_FILES ['file'] ['size']  > 0) AND (in_array($_FILES ['file'] ['type'], $pic_type)) ) {
+	if (($_FILES ['file'] ['size']  > 0)) {
 		$file_name = rand ( 1000, 100000 ) . "-" . $_FILES ['file'] ['name'];
 		$file_loc = $_FILES ['file'] ['tmp_name'];
 		$file_type = $_FILES ['file'] ['type'];
 		$folder = "../public/photos/";
-		echo $file_name;
-		echo $file_loc;
-		echo $folder;
 		
+		$file_type1=substr($file_type,6);
+		//echo $file_type1;
+		if(in_array($file_type1, $pic_type)){
+			
+	
 		move_uploaded_file ( $file_loc, $folder . $file_name );
 		$image = mysqli_query ( $connection, "select * FROM `profile_pic` WHERE user_id='" . $id . "'" );
 		$img = mysqli_fetch_array ( $image );
 		
-		echo $img ['id'];
+		
 		if ($img ['id']) {
 			$upload = mysqli_query ( $connection, "UPDATE profile_pic set profile_pic='" . $file_name . "',user_id='" . $id . "' WHERE user_id = '" . $id . "'" );
-			//echo "kk";
+	
 			if (! $upload) {
 				$error4 = mysqli_error ( $connection );
 				$error4 = explode ( ";", $error4, 3 );
@@ -144,7 +150,7 @@ if (isset ( $_POST ['update'] )) {
 		} else {
 			
 			$upload = mysqli_query ( $connection, "INSERT INTO profile_pic(profile_pic,user_id) VALUES('$file_name','$id')" );
-			echo "jj";
+		
 			if (! $upload) {
 				$error5 = mysqli_error ( $connection );
 				$error5 = explode ( ";", $error5, 3 );
@@ -155,6 +161,7 @@ if (isset ( $_POST ['update'] )) {
 	else{
 		$_SESSION ['message'] = "Profile image can't uploaded.Check files";
 		header("location:../views/update_profile.html?id=".$id);
+	}
 	}
 
 	if ($error1 || $error2 || $error3 || $error4 || $error5) {
@@ -172,7 +179,7 @@ if (isset ( $_POST ['update'] )) {
 
 		header ( "Location: ../views/update_profile.html?id=".$id );
 	}
-}
+
 }
 mysqli_commit($connection);
 
