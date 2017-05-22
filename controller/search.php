7d254aea5feba97757a3ee7b1f2e4c7e6c574439
snaaz <snaaz021@gmail.com>
2017-05-22 12:01:34 +0530

    22nd May

--- controller/search.php
+++ controller/search.php
@@ -1,49 +1,82 @@
 <?php
 include ('connection.php');
+//session_start();
+ //$search_result=mysqli_query($connection, "select 1 from dual ");
+ //echo mysqli_num_rows($search_result);
+// print_r($search_result);
 
 
- 
 
-if(isset( $_POST['search'])){
+$queryArray = array();
+$goodQuery = true ;
+
+$record_available = false;
+
+
+if (isset ( $_GET ['search-key'] )) {
+	$key = $_GET ['search-key'];
 	
-	$search_values=$_POST['search-value'];
-	$id = $_POST['id'];
-	echo $search_values;
 	
+	$id = $_SESSION['id'];
+	// echo $search_values;
 	
 	// Details of Login user........//
-	$user1 = mysqli_query ( $connection, "select * FROM `users` WHERE id= '".$id."'"  );
-	$row1 = mysqli_fetch_array ( $user1);
+	$user = mysqli_query ( $connection, "select * FROM `users` WHERE id= '" . $id . "'" );
+	$row = mysqli_fetch_array ( $user );
 	
-	
-	//search teacher and student details......//
-	if($row1['usertype']=='student'){
-		$search_users1 = mysqli_query ( $connection, "select users.name,users.locality,users.city,users.class,user_subjects.subject_name
+	// search teacher and student details......//
+	if ($row ['usertype'] == 'student') 
+	{
+		$search_users = mysqli_query ( $connection, "select users.name,users.locality,users.city,users.class,user_subjects.subject_name
                                                       from users inner join user_subjects on users.id=user_subjects.user_id
                                                      where users.usertype='teacher' and
-(user_subjects.subject_name like '%".$search_values."%' or users.locality like '%".$search_values."%' or 
-				users.city like '%".$search_values."%' or users.class like '%".$search_values."%')"  );
+(user_subjects.subject_name like '%" . $key . "%' or users.locality like '%" . $key . "%' or 
+				users.city like '%" . $key . "%' or users.class like '%" . $key . "%')" );
 		
-	//	echo "aa";
-		$row1 = mysqli_fetch_array ( $search_users1);
-		echo $row1['name'];
-		echo $row1['subject_name'];
-		//header ( "Location: ../views/search.html");
 	
-	}
-	else{
-		$search_users1 = mysqli_query ( $connection, "select users.name,users.locality,users.city,users.class,user_subjects.subject_name
+		
+	} 
+	else 
+	{
+		$search_users = mysqli_query ( $connection, "select users.name,users.locality,users.city,users.class,user_subjects.subject_name
                                                       from users inner join user_subjects on users.id=user_subjects.user_id
                                                      where users.usertype='teacher' and
-(user_subjects.subject_name like '%".$search_values."%' or users.locality like '%".$search_values."%' 
-				or users.city like '%".$search_values."%' or users.class like '%".$search_values."%')"  );
-	
-		$row1 = mysqli_fetch_array ( $search_users);
-		echo "bb";
-		//header ( "Location: ../views/search.html");
+(user_subjects.subject_name like '%" . $key . "%' or users.locality like '%" . $key . "%' 
+				or users.city like '%" . $key . "%' or users.class like '%" . $key . "%')" );
+		
 	}
 	
+ 	if (mysqli_num_rows ( $search_users ) > 0) 
+ 	{
+ 		$record_available = true;
+		while( $search_user = mysqli_fetch_array ( $search_users )) 
+		{
+			$queryArray[] = $search_user;
+			
+				
+	     }
+	}
+	if($record_available){
+	//echo "session";
+		//$_SESSION['search_output'] = $queryArray;
+		print_r($queryArray[0]['name']);
+	
+	//	print_r($_SESSION['search_output'][0]['name']);
+		
+		//header("Location: ../views/search.html");
+	
+		//exit;
+	}
+	else 
+	{
+		echo "no";
+		echo $_SESSION ["message"] = "No Such Records Found";
+		//header ( "Location: ../views/search.html" );
+	}
 }
 
 $connection->close ();
 ?>
+
+
+
