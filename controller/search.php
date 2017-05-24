6486aafc48df09c694f899464bab57e0d7bc2813
snaaz <snaaz021@gmail.com>
2017-05-24 17:41:47 +0530

    24th May

--- controller/search.php
+++ controller/search.php
@@ -1,6 +1,6 @@
 <?php
 include ('connection.php');
-
+$num_rec_per_page=2;
 $record_available = false;
 
 if (isset ( $_GET ['search-key'] )) {
@@ -11,23 +11,56 @@ if (isset ( $_GET ['search-key'] )) {
 	$user = mysqli_query ( $connection, "select * FROM `users` WHERE id= '" . $id . "'" );
 	$row = mysqli_fetch_array ( $user );
 	
+	
 	// search teacher and student details......//
 	if ($row ['usertype'] == 'student') {
-		$search_users = mysqli_query ( $connection, "select users.name,users.locality,users.city,users.pincode,users.class,user_subjects.subject_name
+		$search_users = mysqli_query ( $connection, "select users.id,users.name,users.locality,users.city,users.pincode,users.class,user_subjects.subject_name
                                                       from users inner join user_subjects on users.id=user_subjects.user_id
                                                      where users.usertype='teacher' and
 (user_subjects.subject_name like '%" . $key . "%' or users.locality like '%" . $key . "%' or 
-				users.city like '%" . $key . "%' or users.class like '%" . $key . "%')" );
+				users.city like '%" . $key . "%' or users.class like '%" . $key . "%') " );
 	} else {
-		$search_users = mysqli_query ( $connection, "select users.name,users.locality,users.city,users.class,user_subjects.subject_name
+		$search_users = mysqli_query ( $connection, "select users.id,users.name,users.locality,users.city,users.class,user_subjects.subject_name
                                                       from users inner join user_subjects on users.id=user_subjects.user_id
-                                                     where users.usertype='teacher' and
+                                                     where users.usertype='student' and
 (user_subjects.subject_name like '%" . $key . "%' or users.locality like '%" . $key . "%' 
-				or users.city like '%" . $key . "%' or users.class like '%" . $key . "%')" );
+				or users.city like '%" . $key . "%' or users.class like '%" . $key . "%') " );
 	}
 	
-	if (mysqli_num_rows ( $search_users ) > 0) {
+	$total_records = mysqli_num_rows ( $search_users );
+	if ($total_records > 0) {
 		$record_available = true;
+		$total_pages = ceil($total_records / $num_rec_per_page);
+		
+	
+		//no-of-records per page should be 10....//
+		if (isset($_GET["page"]))
+		{
+			$page  = $_GET["page"];
+			//echo $page;
+		}
+		else
+		{
+			$page=1;
+		}
+		$start_from = ($page-1) * $num_rec_per_page;
+		//echo $start_from;
+		
+		if ($row ['usertype'] == 'student') {
+			$search_users_perpage = mysqli_query ( $connection, "select users.id,users.name,users.locality,users.city,users.pincode,users.class,user_subjects.subject_name
+                                                      from users inner join user_subjects on users.id=user_subjects.user_id
+                                                     where users.usertype='teacher' and
+(user_subjects.subject_name like '%" . $key . "%' or users.locality like '%" . $key . "%' or
+				users.city like '%" . $key . "%' or users.class like '%" . $key . "%')  LIMIT $start_from, $num_rec_per_page" );
+		} else {
+			$search_users_perpage = mysqli_query ( $connection, "select users.id, users.name,users.locality,users.city,users.class,user_subjects.subject_name
+                                                      from users inner join user_subjects on users.id=user_subjects.user_id
+                                                     where users.usertype='student' and
+(user_subjects.subject_name like '%" . $key . "%' or users.locality like '%" . $key . "%'
+				or users.city like '%" . $key . "%' or users.class like '%" . $key . "%')  LIMIT $start_from, $num_rec_per_page " );
+		}
+		
+		
 	} 
 
 	else {
